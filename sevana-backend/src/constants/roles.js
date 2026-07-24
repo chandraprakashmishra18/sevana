eb90c0a599c0ce8e9b24acf2ec79925feff1907e
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-25 02:18:54 +0530

    6

---
+++ sevana-backend/src/constants/roles.js
@@ -0,0 +1,10 @@
+const ROLES = Object.freeze({
+  USER: "USER",
+  VOLUNTEER: "VOLUNTEER",
+  VET: "VET",
+  NGO: "NGO",
+  ADMIN: "ADMIN",
+  SUPER_ADMIN: "SUPER_ADMIN",
+});
+
+module.exports = ROLES;
