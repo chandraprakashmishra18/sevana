305c6f5167f55de1bea84c14070788beaad036f3
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-25 02:36:52 +0530

    6

---
+++ sevana-backend/src/utils/jwt.util.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const jwtConfig = require("../config/jwt");
+
+function generateAccessToken(payload) {
+  return jwt.sign(
+    payload,
+    jwtConfig.accessToken.secret,
+    {
+      expiresIn: jwtConfig.accessToken.expiresIn,
+    }
+  );
+}
+
+function generateRefreshToken(payload) {
+  return jwt.sign(
+    payload,
+    jwtConfig.refreshToken.secret,
+    {
+      expiresIn: jwtConfig.refreshToken.expiresIn,
+    }
+  );
+}
+
+function verifyAccessToken(token) {
+  return jwt.verify(token, jwtConfig.accessToken.secret);
+}
+
+function verifyRefreshToken(token) {
+  return jwt.verify(token, jwtConfig.refreshToken.secret);
+}
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+};
