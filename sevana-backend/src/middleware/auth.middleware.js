2e8239723a4b7c696f1b3a282f50de03386f184a
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-26 01:29:55 +0530

    2

--- sevana-backend/src/middleware/auth.middleware.js
+++ sevana-backend/src/middleware/auth.middleware.js
@@ -2,6 +2,8 @@ const ApiError = require("../errors/api.error");
 const { verifyAccessToken } = require("../utils/jwt.util");
 
 function requireAuth(req, res, next) {
+  console.log("Authorization Header:", req.headers.authorization);
+
   try {
     const authHeader = req.headers.authorization;
 
@@ -14,12 +16,18 @@ function requireAuth(req, res, next) {
 
     const token = authHeader.split(" ")[1];
 
+    console.log("Token:", token);
+
     const decoded = verifyAccessToken(token);
+
+    console.log("Decoded:", decoded);
 
     req.user = decoded;
 
     next();
   } catch (error) {
+    console.error(error);
+
     next(
       new ApiError({
         statusCode: 401,
