97dac798f522cf886000f2070e2377e889ca74ff
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-28 18:24:55 +0530

    5

--- sevana-backend/src/routes/user.routes.js
+++ sevana-backend/src/routes/user.routes.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const { myStats } = require('../controllers/user.controller');
-const { requireAuth } = require('../middleware/auth.middleware');
-const asyncHandler = require('../utils/async-handler');
+const express = require("express");
+const {
+  getMyProfile,
+  myStats,
+} = require("../controllers/user.controller");
+const { requireAuth } = require("../middleware/auth.middleware");
+const asyncHandler = require("../utils/async-handler");
 
 const router = express.Router();
 
+router.get("/me", requireAuth, asyncHandler(getMyProfile));
+
 // Powers the 3 stat tiles on HomeScreen
-router.get('/me/stats', requireAuth, asyncHandler(myStats));
+router.get("/me/stats", requireAuth, asyncHandler(myStats));
 
 module.exports = router;
