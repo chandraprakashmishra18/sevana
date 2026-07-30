00e0ef13d595aaaa9ad367da79c19db355fa1ad2
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-31 02:29:30 +0530

    52

--- sevana-files/src/screens/home/HomeScreen.jsx
+++ sevana-files/src/screens/home/HomeScreen.jsx
@@ -1,8 +1,19 @@
 import HeroCard from "../../components/Home/HeroCard";
+import XPCard from "../../components/Home/XPCard";
 import { useAuth } from "../../context/AuthContext";
 
 export default function HomeScreen() {
   const { user } = useAuth();
 
-  return <HeroCard user={user} />;
+  return (
+    <>
+      <HeroCard user={user} />
+
+      <XPCard
+        xp={user?.xp || 250}
+        level={user?.level || 1}
+        nextLevelXP={500}
+      />
+    </>
+  );
 }
