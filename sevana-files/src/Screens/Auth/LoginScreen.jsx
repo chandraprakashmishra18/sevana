6514f82d596c3b1c2eac253accf09f03fe5fcd65
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-07-30 16:47:11 +0530

    Login UI changes

--- sevana-files/src/Screens/Auth/LoginScreen.jsx
+++ sevana-files/src/Screens/Auth/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../../Context/AuthContext";
+import Input from "../../Components/Common/Input";
 
 export default function LoginScreen({ onSwitch }) {
   const { signIn } = useAuth();
