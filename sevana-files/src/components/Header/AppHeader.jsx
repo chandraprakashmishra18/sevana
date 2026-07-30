82b1746ed733841dcd02c523cd52005df2740cde
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-31 02:18:21 +0530

    25

---
+++ sevana-files/src/components/Header/AppHeader.jsx
@@ -0,0 +1,17 @@
+import "./AppHeader.css";
+
+export default function AppHeader() {
+  return (
+    <header className="app-header">
+
+      <div className="header-logo">
+        🐾 Sevana
+      </div>
+
+      <div className="header-profile">
+        👤
+      </div>
+
+    </header>
+  );
+}
