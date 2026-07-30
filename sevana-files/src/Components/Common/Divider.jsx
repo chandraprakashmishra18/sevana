42d903de860e3ca81a6209c140fac3ec67842581
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-30 16:34:40 +0530

    45

--- sevana-files/src/Components/Common/Divider.jsx
+++
@@ -1,4 +0,0 @@
-import { T } from '../../Styles/Theme';
-export default function Divider({ dark=false, margin='12px 0' }) {
-  return <div style={{ height:1, background: dark ? '#1E3024' : T.borderLt, margin }} />;
-}
