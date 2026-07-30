3f59ef524311f3316f69f23ae205fd56839ffb5a
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-30 16:36:51 +0530

    4

--- sevana-files/src/main.jsx
+++ sevana-files/src/main.jsx
@@ -2,18 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
-
 import App from "./App";
 
 import { AuthProvider } from "./context/AuthContext";
-import { UserProvider } from "./context/UserContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <App />
     </AuthProvider>
   </StrictMode>
 );
