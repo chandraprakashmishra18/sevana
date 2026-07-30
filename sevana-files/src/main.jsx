465b0ab28b8af4db21d360e3d70eee23f87d541f
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-31 01:59:06 +0530

    5

--- sevana-files/src/main.jsx
+++ sevana-files/src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import App from "./App";
+
+import { AuthProvider } from "./context/AuthContext";
+
 import "./styles/variables.css";
 import "./styles/global.css";
 import "./styles/components.css";
 import "./styles/auth.css";
-
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
