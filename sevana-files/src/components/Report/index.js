439ec41da3145da61c6050ff1ffccdd0d6f61ba6
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-31 17:16:07 +0530

    5

---
+++ sevana-files/src/components/Report/index.js
@@ -0,0 +1,4 @@
+export { default as AnimalTypeSelect } from "./AnimalTypeSelect";
+export { default as SeveritySelector } from "./SeveritySelector";
+export { default as GenderSelector } from "./GenderSelector";
+export { default as LocationPicker } from "./LocationPicker";
