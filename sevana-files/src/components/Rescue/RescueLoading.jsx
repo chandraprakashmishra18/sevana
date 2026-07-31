6c9bc86020c4b41a2b61c1266016836418f8c480
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-31 21:22:46 +0530

    5

---
+++ sevana-files/src/components/Rescue/RescueLoading.jsx
@@ -0,0 +1,15 @@
+export default function RescueLoading() {
+  return (
+    <div className="rescue-loading">
+
+      <div className="loading-spinner" />
+
+      <h3>Loading rescue reports...</h3>
+
+      <p>
+        Looking for nearby animals.
+      </p>
+
+    </div>
+  );
+}
