6b16116a5810a1dc42b5d21318b0260366de948c
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-02 02:20:47 +0530

    5

--- sevana-files/src/components/Common/Card/Card.jsx
+++ sevana-files/src/components/Common/Card/Card.jsx
@@ -1,16 +1,25 @@
 import "./Card.css";
 
 export default function Card({
-  children,
-  className = "",
-  onClick,
-}) {
-  return (
-    <div
-      className={`sevana-card ${className}`}
-      onClick={onClick}
-    >
-      {children}
-    </div>
-  );
+
+    children,
+
+    className=""
+
+}){
+
+    return(
+
+        <div
+
+            className={`card ${className}`}
+
+        >
+
+            {children}
+
+        </div>
+
+    );
+
 }
