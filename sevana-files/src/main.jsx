59c11f6a448235f1a4d871a4b40b0670c4aadcd1
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-01 23:08:02 +0530

    5

--- sevana-files/src/main.jsx
+++ sevana-files/src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 import "./styles/variables.css";
 import "./styles/global.css";
@@ -9,16 +13,17 @@ import "./styles/auth.css";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import { ToastProvider } from "./context/ToastContext";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <ToastProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
           <App />
-        </ToastProvider>
-      </AuthProvider>
-    </BrowserRouter>
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
