042f0f11ba56be177832dd57ce572b58eeea0c9c
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-02 23:44:05 +0530

    5525

--- sevana-files/src/components/Card/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children, className = "", ...props }) {
-  return (
-    <section className={`card ${className}`.trim()} {...props}>
-      {children}
-    </section>
-  );
-}
